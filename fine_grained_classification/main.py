ace621b2e35b0892d183ef2778cfd1b5dddd0f4a
Muhammad Maaz <mmaaz60@gmail.com>
2021-02-18 21:09:08 +0400

    Adds main.py

---
+++ fine_grained_classification/main.py
@@ -0,0 +1,29 @@
+from fine_grained_classification.config.config import Configuration as config
+from fine_grained_classification.dataloader.common import Dataloader
+from fine_grained_classification.model.common import Model
+from fine_grained_classification.train.common import Trainer
+
+if __name__ == "__main__":
+    config.load_config("./config.yml")
+    # Create the dataloaders
+    dataloader = Dataloader(config=config)
+    train_loader, test_loader = dataloader.get_loader()
+    # Create the model
+    model = Model(config=config).get_model()
+    # Create the trainer
+    trainer = Trainer(config=config, model=model, dataloader=train_loader, val_dataloader=test_loader).get_trainer()
+    trainer.train_and_validate()
+
+    # iterations = 0
+    # with torch.no_grad():
+    #     for train_sample, test_sample in zip(train_loader, test_loader):
+    #         images, label = train_sample
+    #         images = images.to("cuda")
+    #         label = label.to("cuda")
+    #         start = time.time()
+    #         logits = model(images)
+    #         print(f"Batch Inference Time: {(time.time() - start) * 1000} ms")
+    #         iterations += 1
+    #         if iterations == 10:
+    #             break
+    # print("done")

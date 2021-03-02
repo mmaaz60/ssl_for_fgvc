560531a53607af0c52a2594ecb68d2d238c119f3
Muhammad Maaz <mmaaz60@gmail.com>
2021-03-02 16:59:36 +0400

    Updates the code to specify train and test transformation in the configuration

--- dataloader/cub_200_2011.py
+++ dataloader/cub_200_2011.py
@@ -1,5 +1,7 @@
 from dataset.cub_200_2011 import Cub2002011 as Dataset
 from torch.utils.data import DataLoader
+from torchvision import transforms
+from fine_grained_classification.utils.utils import get_object_from_path
 
 
 class Cub2002011:
@@ -8,38 +10,62 @@ class Cub2002011:
         The function parse the configuration parameters and load the CUB_200_2011 dataset
         :param config: YML configuration object
         """
-        self.data_root_directory = config.cfg["dataloader"]["root_directory_path"]  # Dataset root directory path
-        self.resize_width = config.cfg["dataloader"]["resize_width"]  # Image resize width
-        self.resize_height = config.cfg["dataloader"]["resize_height"]  # Image resize height
-        self.batch_size = config.cfg["dataloader"]["batch_size"]  # Batch size for the dataloader
-        self.shuffle = config.cfg["dataloader"]["shuffle"]  # Either to shuffle dataset or not
-        self.num_workers = config.cfg["dataloader"]["num_workers"]  # Number of workers to load the dataset
-        self.download = config.cfg["dataloader"]["download"]  # Either to download the dataset or not
+        self.config = config
+        self.train_transform = None  # Train transform
+        self.test_transform = None  # Test transform
         self.train_dataset = None  # Train dataset
         self.test_dataset = None  # Test Dataset
+        self.get_transforms()  # Parse the train and test transforms from configuration file
         self.load_dataset()  # Load the train and test datasets
+
+    def get_transforms(self):
+        train_transforms = self.config.cfg["dataloader"]["transforms"]["train"]
+        test_transforms = self.config.cfg["dataloader"]["transforms"]["test"]
+        self.train_transform = transforms.Compose(
+            [
+                get_object_from_path(train_transforms[i]['path'])(**train_transforms[i]['param'])
+                if 'param' in train_transforms[i].keys()
+                else get_object_from_path(train_transforms[i]['path'])() for i in train_transforms.keys()
+            ]
+        )
+        self.test_transform = transforms.Compose(
+            [
+                get_object_from_path(test_transforms[i]['path'])(**test_transforms[i]['param'])
+                if 'param' in test_transforms[i].keys()
+                else get_object_from_path(test_transforms[i]['path'])() for i in test_transforms.keys()
+            ]
+        )
 
     def load_dataset(self):
         """
         Load the train and test datasets
         """
+        # Parse configuration
+        data_root_directory = self.config.cfg["dataloader"]["root_directory_path"]  # Dataset root directory path
+        resize_width = self.config.cfg["dataloader"]["resize_width"]  # Image resize width
+        resize_height = self.config.cfg["dataloader"]["resize_height"]  # Image resize height
+        download = self.config.cfg["dataloader"]["download"]  # Either to download the dataset or not
         # Load the train dataset
-        self.train_dataset = Dataset(root=self.data_root_directory, train=True,
-                                     resize_dims=(self.resize_width, self.resize_height), download=self.download)
+        self.train_dataset = Dataset(root=data_root_directory, train=True, resize_dims=(resize_width, resize_height),
+                                     transform=self.train_transform, download=download)
         # Load the test dataset
-        self.test_dataset = Dataset(root=self.data_root_directory, train=False,
-                                    resize_dims=(self.resize_width, self.resize_height), download=self.download)
+        self.test_dataset = Dataset(root=data_root_directory, train=False, resize_dims=(resize_width, resize_height),
+                                    transform=self.test_transform, download=download)
 
     def get_dataloader(self):
         """
         Create and return train and test dataloaders
         :return: train_dataloader, test_dataloader
         """
+        # Parse configuration
+        batch_size = self.config.cfg["dataloader"]["batch_size"]  # Batch size for the dataloader
+        shuffle = self.config.cfg["dataloader"]["shuffle"]  # Either to shuffle dataset or not
+        num_workers = self.config.cfg["dataloader"]["num_workers"]  # Number of workers to load the dataset
         # Create the train dataloader
-        train_dataloader = DataLoader(dataset=self.train_dataset, batch_size=self.batch_size, shuffle=self.shuffle,
-                                      num_workers=self.num_workers)
+        train_dataloader = DataLoader(dataset=self.train_dataset, batch_size=batch_size, shuffle=shuffle,
+                                      num_workers=num_workers)
         # Create the test dataloader
-        test_dataloader = DataLoader(dataset=self.test_dataset, batch_size=self.batch_size, shuffle=self.shuffle,
-                                     num_workers=self.num_workers)
+        test_dataloader = DataLoader(dataset=self.test_dataset, batch_size=batch_size, shuffle=shuffle,
+                                     num_workers=num_workers)
         # Return train and test dataloader
         return train_dataloader, test_dataloader

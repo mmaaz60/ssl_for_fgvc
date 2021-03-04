3647b6123c40007fed3c131ba95edb7c13cce865
Muhammad Maaz <mmaaz60@gmail.com>
2021-03-04 23:15:40 +0400

    Adds script to install docker

---
+++ scripts/install_docker.sh
@@ -0,0 +1,17 @@
+sudo apt-get update
+sudo apt-get --no-install-recommends install -y \
+  apt-transport-https \
+  ca-certificates \
+  curl \
+  gnupg-agent \
+  software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+  "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) \
+  stable"
+sudo apt-get update
+  sudo apt-get --no-install-recommends install -y docker-ce docker-ce-cli containerd.io
+
+sudo groupadd docker
+sudo usermod -aG docker "$USER"

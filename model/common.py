6ca6dfa9505dc2a9d9ec0c8382533856d4619ec9
Muhammad Maaz <mmaaz60@gmail.com>
2021-03-08 14:55:54 +0400

    Adds FGVC Resnet

--- model/common.py
+++ model/common.py
@@ -9,9 +9,11 @@ class Model:
         # Select the correct model
         if config.cfg["model"]["name"] == "torchvision":
             from model.torchvision import TorchVision as Model
+        elif config.cfg["model"]["name"] == "fgvc_resnet":
+            from model.fgvc_resnet import FGVCResnet as Model
         else:
             print(f"Please provide correct model to use in configuration. "
-                  f"Available options are ['torchvision']")
+                  f"Available options are ['torchvision', fgvc_resnet]")
             sys.exit(1)
         # Initialize the selected DataLoader
         self.model = Model(config)

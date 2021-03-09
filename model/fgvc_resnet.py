969374ec772426d4580a43e402e74423bb22452c
HanoonaR <hanoonabintrasheed@gmail.com>
2021-03-09 16:16:31 +0400

    Updates FGVCResnet network to include diversification block

--- model/fgvc_resnet.py
+++ model/fgvc_resnet.py
@@ -1,7 +1,6 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
-from scipy.stats import bernoulli
+from layers.diversification_block import DiversificationBlock
 from utils.util import get_object_from_path
 
 
@@ -12,70 +11,14 @@ class FGVCResnet(nn.Module):
         self.model_function = get_object_from_path(config.cfg["model"]["model_function_path"])  # Model type
         self.pretrained = config.cfg["model"]["pretrained"]  # Either to load weights from pretrained model or not
         self.num_classes = config.cfg["model"]["classes_count"]  # Number of classes
+        self.kernel_size = config.cfg["diversification_block"]["patch_size"]
+        self.alpha = config.cfg["diversification_block"]["alpha"]
         self.cam = CAM(self.model_function, self.num_classes, self.pretrained)
-
-    @staticmethod
-    def __diversification_block(cams, kernel_size, alpha):
-        num_classes = len(cams)
-        activation_all_classes = []
-        for i in range(num_classes):
-            activation = cams[i]
-            rc = activation.max().item()
-            p_peak = bernoulli.rvs(rc, size=1)  # Bernoulli prob for P peak: 0 or 1 randomly for c classes
-            b, c, m, n = activation.shape
-            bc = torch.zeros(m, n)  # Initializing Final suppression mask
-
-            # Peak Suppression
-            max_ind = ((activation == activation.max()).nonzero(as_tuple=False))
-            pc = bc
-            pc[max_ind[:, 0], max_ind[:, 1]] = 1
-            bc_dash = p_peak * pc  # Peak suppression mask
-
-            # Patch suppression
-            # patching image to G*G patches
-            kernel_size = kernel_size  # G
-            stride = kernel_size  # G*G patch
-            patches = activation.unfold(2, kernel_size, stride).unfold(3, kernel_size, stride)
-            # num_patches = patches.shape[2] * patches.shape[3]
-            l, m = patches.shape[2], patches.shape[3]
-
-            # Patching mask bc''(Bc_dd) to G*G patches (Bc_patch)
-            # p_patch by probability of patch wise maximums
-            max_vals = torch.max(patches[0][0], 3)
-            max_vals = torch.max(max_vals.values, 2)
-            p_patch = max_vals.values.cpu().apply_(lambda x: (bernoulli.rvs(x, size=1)))
-            p_patch_ind = torch.nonzero(torch.tensor(p_patch) == 1, as_tuple=False)
-            bc_dd = bc  # Bc double dash allocating size with zeros
-            bc_patch = bc_dd.unsqueeze(0).unsqueeze(0).unfold(2, kernel_size, stride).unfold(3, kernel_size, stride)
-            # bc_patch[0][0][P_patch_ind[:, 0]][P_patch_ind[:, 1]]=1 # Implementation of bc'' without for loop
-            bc_patch[0][0] = 0
-            for i in range(len(p_patch_ind)):
-                bc_patch[0][0][p_patch_ind[i][0]][p_patch_ind[i][1]] = 1
-
-            # bc_patch folding back Bc''(Bc_dd)
-            bc_patch = (bc_patch.reshape(1, 1, l, m, kernel_size * kernel_size)).permute(0, 1, 4, 2, 3).squeeze(
-                0).squeeze(
-                0)
-            bc_patch = bc_patch.view(kernel_size * kernel_size, -1)
-            bc_dd = F.fold(bc_patch.unsqueeze(0), (m, n), kernel_size=kernel_size, stride=stride)
-            bc_dd = bc_dd.squeeze(0).squeeze(0)
-
-            # bc'' not supressing peak(peak=0)
-            bc_dd[max_ind[:, 0], max_ind[:, 1]] = 0
-
-            # Final Supression Mask Bc
-            bc = bc_dash + bc_dd
-
-            # Activation Supression Factor
-            supress_ind = ((bc == 1).nonzero(as_tuple=False))
-            activation[0][supress_ind[:, 0], supress_ind[:, 1]] *= alpha
-            activation_all_classes = torch.stack(activation.unsqueeze(1), dim=0)
-
-        return activation_all_classes
+        self.diversification_block = DiversificationBlock(self.kernel_size, self.alpha)
 
     def forward(self, x):
         out = self.cam(x)
-        # out = self.__diversification_block(out[0], 20, 0.1)
+        out = self.diversification_block(out)
         out = out.mean([2, 3])
 
         return out

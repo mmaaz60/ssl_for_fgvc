480c7c8513daf32443ac3a339817378fbcf72b5f
hanoonaR <hanoonabintrasheed@gmail.com>
2021-03-10 19:37:02 +0400

    Updates implementation

--- layers/diversification_block.py
+++ layers/diversification_block.py
@@ -4,7 +4,7 @@ import torch.nn.functional as F
 
 
 class DiversificationBlock(nn.Module):
-    def __init__(self, kernel_size, alpha, p_peak=0.5, p_patch=0.5, device="cuda"):
+    def __init__(self, kernel_size=3, alpha=0.1, p_peak=0.5, p_patch=0.5, device="cuda"):
         super(DiversificationBlock, self).__init__()
         self.kernel_size = kernel_size
         self.alpha = alpha
@@ -31,7 +31,7 @@ class DiversificationBlock(nn.Module):
                                                        device=self.device), torch.tensor(self.p_patch)))
         bc_dd = torch.zeros_like(patches)  # Mask for peaks for each class
         bc_dd[p_patch == 1] = 1
-        bc_dd = (bc_dd.reshape(b, c, l, k, kernel_size * kernel_size)).permute(0, 1, 4, 2, 3)
+        bc_dd = (bc_dd.reshape(b, c, l, k, kernel_size * gkernel_size)).permute(0, 1, 4, 2, 3)
         bc_dd = bc_dd.reshape(b, c, kernel_size * kernel_size, -1)
         bc_dd_batch = torch.zeros_like(activation)
         for i in range(b):

e7b2433487e6c8d3873a8a43c1595f4eca2f3847
hanoonaR <hanoonabintrasheed@gmail.com>
2021-03-21 15:07:53 +0400

    Supports cub_200_2011_contrastive dataset

--- dataloader/common.py
+++ dataloader/common.py
@@ -9,9 +9,11 @@ class Dataloader:
         # Select the correct model
         if config.cfg["dataloader"]["name"] == "cub_200_2011":
             from dataloader.cub_200_2011 import Cub2002011 as DataLoader
+        elif config.cfg["dataloader"]["name"] == "cub_200_2011_contrastive":
+            from dataloader.cub_200_2011_contrastive import Cub2002011Contrastive as DataLoader
         else:
             print(f"Please provide correct dataloader to use in configuration. "
-                  f"Available options are ['cub_200_2011']")
+                  f"Available options are ['cub_200_2011', 'cub_200_2011_contrastive']")
             sys.exit(1)
         # Initialize the selected DataLoader
         self.dataloader = DataLoader(config)

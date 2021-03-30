f3722e8bd3a94d24ba16fba3e6bdf9aca6455bbc
hanoonaR <hanoonabintrasheed@gmail.com>
2021-03-31 01:51:10 +0400

    Adds model definition and dataset for dcl training

---
+++ dataset/dcl.py
@@ -0,0 +1,59 @@
+import os
+from torchvision.datasets.folder import default_loader
+from dataset.cub_200_2011 import Cub2002011
+from utils.util import get_image_crops
+from PIL import ImageStat
+
+
+class DCL(Cub2002011):
+    def __init__(self, root, train=True, download=True, crop_patch_size=(7, 7), num_classes=200, common_transform=None,
+                 jigsaw_transform=None, final_transform=None, train_data_fraction=1, test_data_fraction=1):
+        """
+        Initialize the class variables, download the dataset (if prompted to do so), verify the data presence,
+        :param root: Dataset root path
+        :param train: Train dataloader flag (True: Train Dataloader, False: Test Dataloader)
+        :param download: Flag set to download the dataset
+        """
+        super().__init__(root=root, train=train, download=download, loader=default_loader, resize_dims=None,
+                         transform=None, train_data_fraction=train_data_fraction, test_data_fraction=test_data_fraction)
+        self.num_classes = num_classes
+        self.crop_patch_size = crop_patch_size
+        self.common_transform = common_transform
+        self.jigsaw_transform = jigsaw_transform
+        self.final_transform = final_transform
+
+    def __getitem__(self, idx):
+        """
+        The function overrides the __getitem__ method of the Cub2002011 class
+        :param idx: The index to fetch the data entry/sample
+        :return: The image tensor and corresponding label
+        """
+        sample = self.data.iloc[idx]
+        path = os.path.join(self.root, self.base_folder, sample.filepath)
+        target = sample.target - 1  # Targets start at 1 by default, so shift to 0
+        img = self.loader(path)  # Call the loader function to load the image
+        if self.train:
+            img_original = self.common_transform(img) if self.common_transform is not None else img
+            img_original_list = get_image_crops(img_original, self.crop_patch_size)
+            original_patch_range = self.crop_patch_size[0] * self.crop_patch_size[1]
+            original_patch_labels = [(i - (original_patch_range // 2)) / original_patch_range
+                                     for i in range(original_patch_range)]
+            img_jigsaw = self.jigsaw_transform(img) if self.jigsaw_transform is not None else img
+            img_jigsaw_list = get_image_crops(img_jigsaw, self.crop_patch_size)
+            original_stats = [sum(ImageStat.Stat(im).mean()) for im in img_original_list]
+            jigsaw_stats = [sum(ImageStat.Stat(im).mean()) for im in img_jigsaw_list]
+            jigsaw_patch_labels = []
+            for jigsaw_stat in jigsaw_stats:
+                distance = [abs(jigsaw_stat - original_stat) for original_stat in original_stats]
+                index = distance.index(min(distance))
+                jigsaw_patch_labels.append(original_patch_labels[index])
+            img_jigsaw = self.final_transform(img_jigsaw) if self.final_transform is not None else img_jigsaw
+            target_jigsaw = target + self.num_classes
+            img_original = self.final_transform(img_original) if self.final_transform is not None else img_original
+            return img_original, img_jigsaw, target, target_jigsaw, original_patch_labels, jigsaw_patch_labels
+        else:
+            # Apply the transforms if the transformations are specified
+            if self.final_transform is not None:
+                img = self.final_transform(img)
+            # Return the image tensor and corresponding target/label
+            return img, target
---
+++ model/torchvision_ssl_dcl.py
@@ -0,0 +1,38 @@
+import torch.nn as nn
+from utils.util import get_object_from_path
+
+
+class TorchVisionSSLDCL(nn.Module):
+    def __init__(self, config):
+        super(TorchVisionSSLDCL, self).__init__()
+        # Parse the configuration parameters
+        self.model_function = get_object_from_path(config.cfg["model"]["model_function_path"])  # Model type
+        self.pretrained = config.cfg["model"]["pretrained"]  # Either to load weights from pretrained model or not
+        self.num_classes = config.cfg["model"]["classes_count"]  # Number of classes
+        # Load the model
+        net = self.model_function(pretrained=self.pretrained)
+        net_list = list(net.children())
+        self.feature_extractor = nn.Sequential(*net_list[:-2])
+        self.avg_pool = nn.AdaptiveAvgPool2d(output_size=1)
+        self.cls_classifier = nn.Linear(in_features=net.fc.in_features, out_features=self.num_classes,
+                                        bias=(net.fc.bias is not None))
+        self.adv_classifier = nn.Linear(in_features=net.fc.in_features, out_features=2 * self.num_classes,
+                                        bias=(net.fc.bias is not None))
+        self.conv_mask = nn.Conv2d(in_channels=net.fc.in_features, out_channels=1, kernel_size=1, stride=1,
+                                   padding=0, bias=True)
+        self.avg_pool_1 = nn.AvgPool2d(2, stride=2)
+        self.flatten = nn.Flatten()
+        self.relu = nn.ReLU()
+
+    def forward(self, x, train=True):
+        feat = self.feature_extractor(x)
+        classifier = self.avg_pool(feat)
+        classifier = self.flatten(classifier)
+        cls_classifier = self.cls_classifier(classifier)
+        adv_classifier = self.adv_classifier(classifier)
+        jigsaw_mask = self.conv_mask(feat)
+        jigsaw_mask = self.avg_pool_1(jigsaw_mask)
+        jigsaw_mask = self.relu(jigsaw_mask)
+        jigsaw_mask = self.flatten(jigsaw_mask)
+
+        return [cls_classifier, adv_classifier, jigsaw_mask]

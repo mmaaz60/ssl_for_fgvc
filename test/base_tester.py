f1c53c1c514108ae0b6a542c3bbf89c5946b7042
Muhammad Maaz <mmaaz60@gmail.com>
2021-04-21 19:27:33 +0400

    Updates the base tester to report top1 and top2 accuracies.

--- test/base_tester.py
+++ test/base_tester.py
@@ -1,7 +1,6 @@
 import torch
 import logging
 
-
 logger = logging.getLogger(f"test/base_tester.py")
 
 
@@ -16,8 +15,8 @@ class BaseTester:
         model.eval()
         with torch.no_grad():
             total_loss = 0
-            total_predictions = 0
-            total_correct_predictions = 0
+            total_correct_predictions_top_1 = 0
+            total_correct_predictions_top_2 = 0
             for batch_idx, d in enumerate(self.dataloader):
                 inputs, labels = d
                 inputs = inputs.to(self.device)
@@ -25,10 +24,16 @@ class BaseTester:
                 outputs = model(inputs, train=False)
                 loss = self.loss(outputs, labels)
                 total_loss += loss
-                _, preds = torch.max(outputs, 1)
-                total_predictions += len(preds)
-                total_correct_predictions += torch.sum(preds == labels.data)
+                top_2_val, top_2_pos = torch.topk(outputs, 2)
+                batch_corrects_1 = torch.sum((top_2_pos[:, 0] == labels)).data.item()
+                total_correct_predictions_top_1 += batch_corrects_1
+                batch_corrects_2 = torch.sum((top_2_pos[:, 1] == labels)).data.item()
+                total_correct_predictions_top_2 += (batch_corrects_2 + batch_corrects_1)
             metrics['loss'] = float(total_loss) / len(self.dataloader)
-            metrics['accuracy'] = float(total_correct_predictions) / total_predictions
-            logger.info(f"Validation loss: {metrics['loss']}, Validation accuracy: {metrics['accuracy']}")
+            metrics['accuracy_top_1'] = float(total_correct_predictions_top_1) / (
+                                        len(self.dataloader) * self.dataloader.batch_size)
+            metrics['accuracy_top_2'] = float(total_correct_predictions_top_2) / (
+                                        len(self.dataloader) * self.dataloader.batch_size)
+            logger.info(f"Validation loss: {metrics['loss']}, Top-1 Validation accuracy: {metrics['accuracy_top_1']}"
+                        f", Top-2 Validation accuracy: {metrics['accuracy_top_2']}")
         return metrics

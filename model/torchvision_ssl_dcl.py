6d6ceebd5ebfb7130bb123c55b13ebd9fb8a8e10
HanoonaR <hanoonabintrasheed@gmail.com>
2021-04-25 23:30:41 +0400

    Minor modifications

--- model/torchvision_ssl_dcl.py
+++ model/torchvision_ssl_dcl.py
@@ -9,7 +9,7 @@ class TorchVisionSSLDCL(nn.Module):
         self.model_function = get_object_from_path(config.cfg["model"]["model_function_path"])  # Model type
         self.pretrained = config.cfg["model"]["pretrained"]  # Either to load weights from pretrained model or not
         self.num_classes = config.cfg["model"]["classes_count"]  # Number of classes
-        self.class_type = config.cfg["dataloader"]["class_type"]
+        self.prediction_type = config.cfg["model"]["prediction_type"]
         jigsaw_size = config.cfg["dataloader"]["transforms"]["jigsaw"]["t_1"]["param"]["size"]
         self.jigsaw_class = jigsaw_size[0] * jigsaw_size[1]
         # Load the model
@@ -23,8 +23,8 @@ class TorchVisionSSLDCL(nn.Module):
                                         bias=(net.fc.bias is not None))
         self.conv_mask = nn.Conv2d(in_channels=net.fc.in_features, out_channels=1, kernel_size=1, stride=1,
                                    padding=0, bias=True)
-        self.conv_mask_cls = nn.Conv2d(in_channels=net.fc.in_features, out_channels=self.jigsaw_class, kernel_size=1, stride=1,
-                                   padding=0, bias=True)
+        self.conv_mask_cls = nn.Conv2d(in_channels=net.fc.in_features, out_channels=self.jigsaw_class, kernel_size=1,
+                                       stride=1, padding=0, bias=True)
         self.avg_pool_1 = nn.AvgPool2d(2, stride=2)
         self.jigsaw_cls_classifier = nn.Linear(in_features=49*7*7, out_features=self.jigsaw_class, bias=True)
         self.flatten = nn.Flatten()
@@ -39,19 +39,17 @@ class TorchVisionSSLDCL(nn.Module):
         cls_classifier = self.cls_classifier(classifier)
         if train:
             adv_classifier = self.adv_classifier(classifier)
-            if self.class_type == "reg":
-                jigsaw_mask = self.conv_mask(feat)
+            jigsaw_mask = self.conv_mask(feat)
+            if self.prediction_type == "regression":
                 jigsaw_mask = self.avg_pool_1(jigsaw_mask)
                 jigsaw_mask = self.tan_h(jigsaw_mask)
                 jigsaw_mask = self.flatten(jigsaw_mask)
             else:
-                jigsaw_mask = self.conv_mask_cls(feat)
                 jigsaw_mask = self.relu(jigsaw_mask)
                 jigsaw_mask = self.avg_pool_1(jigsaw_mask)
                 jigsaw_mask = self.flatten(jigsaw_mask)
                 jigsaw_mask = self.jigsaw_cls_classifier(jigsaw_mask)
-                if self.class_type == "bce":
-                    jigsaw_mask = self.sigmoid(jigsaw_mask)
+                jigsaw_mask = self.sigmoid(jigsaw_mask)
 
             return [cls_classifier, adv_classifier, jigsaw_mask]
         else:

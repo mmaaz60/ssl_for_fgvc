12362fd1e022a4fbe8a40a87c42c6532ec13c89f
Muhammad Maaz <mmaaz60@gmail.com>
2021-04-29 02:01:56 +0400

    Removes redundant files.

--- scripts/cam_visualizations.py
+++ scripts/cam_visualizations.py
@@ -2,11 +2,11 @@ import sys
 import os
 import argparse
 import torch
-from torch import nn
 import numpy as np
 from PIL import Image
-from matplotlib import cm
 from torchvision import transforms
+from pytorch_grad_cam import GradCAM, ScoreCAM, GradCAMPlusPlus, AblationCAM, XGradCAM
+from pytorch_grad_cam.utils.image import show_cam_on_image
 
 # Add the root folder (ssl_for_fgvc) as the path
 sys.path.append(f"{'/'.join(os.getcwd().split('/')[:-1])}")
@@ -17,40 +17,52 @@ from utils.util import get_object_from_path
 
 
 class CAMVisualization:
-    def __init__(self, model):
+    def __init__(self, model, model_name, cam_method='GradCAM'):
         self.model = model.eval()
+        self.model_name = model_name
+        self.cam_method = cam_method
+        self.target_layer = None
         self.cam = None
+        self._set_target_layer()
+        self._set_cam()
 
-    def get_cam_image(self, x, x_orig, topk=1):
+    def _set_target_layer(self):
+        if self.model_name == "torchvision":
+            self.target_layer = self.model.model.layer4[-1]
+        elif self.model_name == "torchvision_ssl_rotation":
+            self.target_layer = self.model.model.layer4[-1]
+        elif self.model_name == "pirl":
+            self.target_layer = self.model.model.layer4[-1]
+        elif self.model_name == "dcl":
+            self.target_layer = self.model.model.layer4[-1]
+
+    def _set_cam(self):
+        if self.cam_method == "GradCAM":
+            self.cam = GradCAM(model=self.model, target_layer=self.target_layer, use_cuda=True)
+        elif self.cam_method == "GradCAMPlusPlus":
+            self.cam = GradCAMPlusPlus(model=self.model, target_layer=self.target_layer, use_cuda=True)
+        elif self.cam_method == "GradCAMPlusPlus":
+            self.cam = ScoreCAM(model=self.model, target_layer=self.target_layer, use_cuda=True)
+        elif self.cam_method == "GradCAMPlusPlus":
+            self.cam = AblationCAM(model=self.model, target_layer=self.target_layer, use_cuda=True)
+        elif self.cam_method == "GradCAMPlusPlus":
+            self.cam = XGradCAM(model=self.model, target_layer=self.target_layer, use_cuda=True)
+        else:
+            self.cam = GradCAM(model=self.model, target_layer=self.target_layer, use_cuda=True)
+
+    def get_cam_image(self, x, x_orig):
         """
         The function interpolates the class activation maps and return an image of required size
         :param x: Batch of images (b, c, h, w)
         """
-        h, w = x_orig.size
-        cam, topk_pred = self.model.get_cam(x, topk)
-        min_val, min_args = torch.min(cam, dim=2, keepdim=True)
-        cam -= min_val
-        max_val, max_args = torch.max(cam, dim=2, keepdim=True)
-        cam /= max_val
-        topk_cam = cam[0][topk_pred[0]].unsqueeze(0)
-        topk_cams = nn.functional.interpolate(topk_cam.unsqueeze(0), (h, w), mode='bilinear',
-                                              align_corners=True).squeeze(0)
-        topk_cams = torch.split(topk_cams, 1)
-        # For loop here if many top cams required
-        cam_ = topk_cams[0].squeeze().cpu().data.numpy()
-        cam_pil = array_to_cam(cam_)
-        blended_cam = blend(x_orig, cam_pil)
+        grayscale_cam = self.cam(input_tensor=x, target_category=1)
+        visualization = show_cam_on_image(np.array(x_orig, dtype=np.float32) / 255.0, grayscale_cam, use_rgb=True)
+        pil_image = Image.fromarray(visualization)
+        # Get the classification label
+        cls_scores = self.model(x)
+        _, label = torch.max(cls_scores, 1)
 
-        return blended_cam, topk_pred[0]
-
-
-def array_to_cam(arr):
-    cam_pil = Image.fromarray(np.uint8(cm.gist_earth(arr) * 255)).convert("RGB")
-    return cam_pil
-
-
-def blend(image1, image2, alpha=0.75):
-    return Image.blend(image1, image2, alpha)
+        return pil_image, int(label.detach())
 
 
 def parse_arguments():
@@ -60,8 +72,9 @@ def parse_arguments():
     ap = argparse.ArgumentParser()
     ap.add_argument("-config", "--config_path", required=True,
                     help="The path to the pipeline .yml configuration file.")
-    ap.add_argument("-topk", "--topk", required=False,
-                    help="The top k predictions to consider.")
+    ap.add_argument("-cam", "--cam_method", required=False, default='GradCAM',
+                    help="Cam method to use. Possible options are "
+                         "[GradCAM, ScoreCAM, GradCAMPlusPlus, AblationCAM, XGradCAM]")
     ap.add_argument("-save", "--output_directory", required=True,
                     help="The path to output directory to save the visualizations.")
     ap.add_argument("-dim", "--output_dim", type=int, required=False, default=448,
@@ -98,7 +111,7 @@ def main():
     checkpoints = torch.load(checkpoints_path)
     model.load_state_dict(checkpoints["state_dict"], strict=True)
     # Create CAM visualizer object
-    visualizer = CAMVisualization(model)
+    visualizer = CAMVisualization(model, config.cfg["model"]["name"], cam_method=args["cam_method"])
     # Create transforms for performing inference
     resize_dim = (config.cfg["dataloader"]["resize_width"], config.cfg["dataloader"]["resize_height"])
     infer_dim = args["output_dim"]
@@ -121,19 +134,16 @@ def main():
         input_trans = torch.unsqueeze(input_trans, 0)
         input_trans = input_trans.to(args["device"])
         # Get the cam image
-        output_image, predicted_label = visualizer.get_cam_image(input_trans, input.resize((infer_dim, infer_dim), Image.ANTIALIAS))
+        output_image, predicted_label = visualizer.get_cam_image(input_trans,
+                                                                 input.resize((infer_dim, infer_dim), Image.ANTIALIAS))
         # Write the cam images to the disc
         predicted_label += 1
         if predicted_label == image_label:
-            if not os.path.exists(f"{args['output_directory']}/correct_predictions/{image_label}_{predicted_label}"):
-                os.mkdir(f"{args['output_directory']}/correct_predictions/{image_label}_{predicted_label}")
-            output_image.save(f"{args['output_directory']}/correct_predictions/{image_label}_{predicted_label}/"
-                              f"{i}.jpg")  # Save the PIL image
+            # Save the PIL image
+            output_image.save(f"{args['output_directory']}/correct_predictions/{i}_{image_label}_{predicted_label}.jpg")
         else:
-            if not os.path.exists(f"{args['output_directory']}/wrong_predictions/{image_label}_{predicted_label}"):
-                os.mkdir(f"{args['output_directory']}/wrong_predictions/{image_label}_{predicted_label}")
-            output_image.save(f"{args['output_directory']}/wrong_predictions/{image_label}_{predicted_label}/"
-                              f"{i}.jpg")  # Save the PIL image
+            # Save the PIL image
+            output_image.save(f"{args['output_directory']}/wrong_predictions/{i}_{image_label}_{predicted_label}.jpg")
 
 
 if __name__ == "__main__":
